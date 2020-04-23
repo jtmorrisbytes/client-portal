ae01c3914e65b6de8326cde8fb5fa9ee914a1781
= <=>
2020-04-23 12:49:08 -0500

    prepare to convert express session to redux

--- server/spec/auth/checkAuthsState.spec.js
+++ server/spec/auth/checkAuthsState.spec.js
@@ -1,17 +1,15 @@
 // const jasmine = require("jasmine");
-const server = require("../../src/index.js");
-if (!server) {
-  throw new Error();
-}
-console.log(server || "nothing");
-const request = require("supertest");
+
+let request = require("supertest");
 async function startAuthSession(done, callback) {
-  request(await server())
+  let server = await require("../../src/index.js")();
+  request(server)
     .post("/api/auth/")
     .expect("Content-Type", /json/)
     .expect(200, (err, res) => {
       if (err) {
         done(err);
+        server = null;
       } else {
         expect(res.body).toEqual(jasmine.any(Object));
         expect(res.body.auth).toEqual(jasmine.any(Object));
@@ -23,17 +21,44 @@ async function startAuthSession(done, callback) {
         expect(auth.state).toEqual(jasmine.any(String));
         expect(auth.state.length).toBeGreaterThan(0);
         expect(user).toBeNull();
-
-        callback(res);
+        if (callback) {
+          callback(res, server);
+        } else {
+          done();
+          server = null;
+        }
       }
     });
 }
 
 describe("When the app tries to authenticate", function () {
   // console.dir(app);
+  const testUser = {
+    firstName: "Jordan",
+    lastName: "Morris",
+    address: "123 Software way",
+    city: "Silicon Valley",
+    state: "California",
+    zip: "12345",
+    phoneNumber: "1234567890",
+  };
+  let toShortPassword = "!a3";
+  let badPassword = "password";
+  let strongPassword = "j}4Sf_td'pQ%";
   it("should be able to POST /", async (done) => {
-    startAuthSession(done, (res) => {
-      done();
+    startAuthSession(done);
+  });
+  it("should be able to register after starting an auth session", (done) => {
+    startAuthSession(done, (res, serverInst) => {
+      request(serverInst)
+        .post("/api/auth/register?state=" + res.body.auth.state)
+        .expect("Content-Type", /json/)
+        .expect(200, (err, registerRes) => {
+          if (err) {
+            console.log(registerRes.body);
+            done(err);
+          }
+        });
     });
   });
 });
