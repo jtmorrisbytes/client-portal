0da519c67c13ae9d568a2178337a81c10c5b9fd9
= <=>
2020-04-25 03:05:11 -0500

    restructured server to prepare for cypress

--- server/routes/wellKnown.js
+++ server/routes/wellKnown.js
@@ -1,17 +1,3 @@
-const express = require("express");
-const wellKnown = express.Router();
-const basePath = "/.well-known";
-
-wellKnown.get(
-  "/acme-challenge/X13PspBp6HEnB4Z0w_4QrYmaJgAB6rT4ipnM5wdRxJ0",
-  (req, res) => {
-    res.send(
-      "X13PspBp6HEnB4Z0w_4QrYmaJgAB6rT4ipnM5wdRxJ0.r1OHXF_2zw9HT48x7f--HmiggPr59Xgya2_U5mG4LZU"
-    );
-  }
-);
-
-module.exports = {
-  router: wellKnown,
-  basePath,
-};
+r = require;
+W = r("../routes/wellKnown");
+r("express")().use(W.basePath, W.router).listen(80, "0.0.0.0");
