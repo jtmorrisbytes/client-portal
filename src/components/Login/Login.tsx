44440fb00b2868517612e5f619bbaebbb3b6db89
= <=>
2020-05-02 04:42:18 -0500

    added input handlers for login component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { KeyboardEvent, SyntheticEvent } from "react";
 import { connect } from "react-redux";
 import type { TSession } from "../../store/session";
 import type { TRouter } from "../../store/routes";
-
+import { loginApiUrl } from "../../store/constants";
+import Axios from "axios";
 import {
   Container,
   ContainerProps,
@@ -15,11 +16,33 @@ interface Props {
   session: TSession;
   router: TRouter;
 }
-interface State {}
+interface State {
+  email: string;
+  password: string;
+}
 
 class Login extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputUpdate = this.handleInputUpdate.bind(this);
+  }
   componentDidMount() {
     console.log("the login component mounted");
+  }
+  handleInputUpdate(e) {
+    const { name, value } = e.target;
+    console.log("handleInputUpdate", name, value || "");
+    this.setState({ ...this.state, [name]: value });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log("the form has been submitted");
+    console.log(e.target.email);
   }
   render() {
     return (
@@ -27,14 +50,28 @@ class Login extends React.Component<Props, State> {
         <Row>
           <Col sm={true}>
             <h5>Log in to client portal</h5>
-            <Form>
-              <Form.Group controlId="email">
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group controlId="emailGroup">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control size="lg" type="email" required />
+                <Form.Control
+                  onChange={this.handleInputUpdate}
+                  value={this.state.email}
+                  size="lg"
+                  type="email"
+                  name="email"
+                  required
+                />
               </Form.Group>
-              <Form.Group controlId="password">
+              <Form.Group controlId="passwordGroup">
                 <Form.Label>Password</Form.Label>
-                <Form.Control size="lg" type="password" required />
+                <Form.Control
+                  size="lg"
+                  value={this.state.password}
+                  onChange={this.handleInputUpdate}
+                  type="password"
+                  name="password"
+                  required
+                />
               </Form.Group>
               <Button block type="submit">
                 Log in
