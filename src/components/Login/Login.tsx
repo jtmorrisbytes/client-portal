650e0eb85e6f24e7b678a92015203f72e1ebeee3
= <=>
2020-05-01 22:45:22 -0500

    added login fields

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,7 +3,14 @@ import { connect } from "react-redux";
 import type { TSession } from "../../store/session";
 import type { TRouter } from "../../store/routes";
 
-import { Container, ContainerProps } from "react-bootstrap";
+import {
+  Container,
+  ContainerProps,
+  Row,
+  Col,
+  Form,
+  Button,
+} from "react-bootstrap";
 interface Props {
   session: TSession;
   router: TRouter;
@@ -15,7 +22,28 @@ class Login extends React.Component<Props, State> {
     console.log("the login component mounted");
   }
   render() {
-    return <Container>welcome to login</Container>;
+    return (
+      <Container>
+        <Row>
+          <Col sm={true}>
+            <h5>Log in to client portal</h5>
+            <Form>
+              <Form.Group controlId="email">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control size="lg" type="email" required />
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control size="lg" type="password" required />
+              </Form.Group>
+              <Button block type="submit">
+                Log in
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
 
