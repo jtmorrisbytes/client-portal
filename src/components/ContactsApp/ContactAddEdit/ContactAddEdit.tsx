02e17c2da3c5b0935b914c653ccfbf9f4166e291
= <=>
2020-05-07 05:30:40 -0500

    time to scaffold and test ComponentAddEdit

---
+++ src/components/ContactsApp/ContactAddEdit/ContactAddEdit.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface Props {}
+interface State {}
+
+class ContactAddEdit extends React.Component<Props, State> {
+  render() {
+    return <div> hello WOrld </div>;
+  }
+}
+
+export default ContactAddEdit;
--- src/routes.tsx
+++ src/routes.tsx
@@ -6,6 +6,7 @@ import Register from "./components/Register/Register";
 import { AppPicker } from "./components/AppPicker/AppPicker";
 
 import ContactsApp from "./components/ContactsApp/ContactsApp";
+import ContactAddEdit from "./components/ContactsApp/ContactAddEdit/ContactAddEdit";
 import HeadNav from "./components/HeadNav/HeadNav";
 import FootNav from "./components/FootNav/FootNav";
 
@@ -16,13 +17,14 @@ export default function Routes(props) {
       <Route path={REGISTER_URL} component={Register} />
       <Route path={LOGIN_URL} component={Login} />
       <Route path={PASSWORD_RESET}></Route>
-      <Route path="/404">this is the not found page</Route>
       <Route path="/401">not authorized</Route>
       <Route path="/500">internal server error</Route>
       <Route path="*">
         <HeadNav />
         <Switch>
+          <Route path="/contacts/add" component={ContactAddEdit} />
           <Route path="/contacts" component={ContactsApp} />
+          <Route>Page not found</Route>
         </Switch>
         <FootNav />
       </Route>
